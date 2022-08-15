35e621934a8e55fc7f9b4f2b111cf81f163881aa
Marisol <mantunezflores@confluent.io>
2022-08-15 10:09:04 -0700

    Add Apache license header

--- clients/src/test/java/org/apache/kafka/clients/DeserializeMetricsDataTest.java
+++ clients/src/test/java/org/apache/kafka/clients/DeserializeMetricsDataTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.kafka.clients;
 
 import io.opentelemetry.proto.metrics.v1.InstrumentationLibraryMetrics;
@@ -16,9 +32,9 @@ import org.junit.jupiter.api.Test;
 
 public class DeserializeMetricsDataTest {
 
-    public static ByteBuffer metricsDataBuffer;
-    public static MetricsData metricsData;
-    public static Metric sampleMetric;
+    public ByteBuffer metricsDataBuffer;
+    public MetricsData metricsData;
+    public Metric sampleMetric;
 
 
     @BeforeEach
